ede38f9c31cbc55a6e3a4ccdb2657a11d7723dfa
Romario Williams <romariowilliams@hotmail.com>
2019-02-11 03:53:01 -0500

    Update Forms component to be stateless

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,15 +1,11 @@
 import React from 'react';
 
-class Forms extends React.Component{
-  render(){
-    return(
-    <form onSubmit={this.props.getWeather}>
-    <input type="text" name="city" placeholder="City"/>
-    <input type="text" name="country" placeholder="Country"/>
-    <button>Get Weather</button>
-    </form>
-    );
-  }
-};
+const Forms = props => (
+  <form onSubmit={props.getWeather}>
+  <input type="text" name="city" placeholder="City"/>
+  <input type="text" name="country" placeholder="Country"/>
+  <button>Get Weather</button>
+  </form>
+);
 
 export default Forms;
