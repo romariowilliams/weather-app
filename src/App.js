9ef6bdf058be6747c23f54bd85b5bb5e79eaf1ab
Romario Williams <romariowilliams@hotmail.com>
2019-02-11 06:03:59 -0500

    Update App.js to only require user to enter city name

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
   state = {
     temperature: undefined,
     city: undefined,
-    country:undefined,
+    country: undefined,
     humidity: undefined,
     description: undefined,
     error: undefined
@@ -17,18 +17,17 @@ class App extends React.Component {
   getWeather = async (e) => {
    e.preventDefault();
    const city = e.target.elements.city.value;
-   const country = e.target.elements.country.value;
-    const api_call  = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const api_call  = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
 
-   if (city && country){
+   if (city){
       	 if(data.name){ // checks if there is data (specifically the city name) fetched from the API
-	
+		console.log(data)
 		this.setState({
 	
        	 	temperature: data.main.temp,
        	 	city: data.name,
-       	 	country: data.sys.country,
+		country: data.sys.country,
         	humidity: data.main.humidity,
         	description: data.weather[0].description,
         	error: ""
@@ -39,7 +38,7 @@ class App extends React.Component {
       		this.setState({
         	temperature: undefined,
         	city: undefined,
-        	country: undefined,
+		country: undefined,
         	humidity: undefined,
         	description: undefined,
         	error: "Sorry, we can't find that city. Try again."
@@ -50,7 +49,7 @@ class App extends React.Component {
       this.setState({
         temperature: undefined,
         city: undefined,
-        country: undefined,
+	country:undefined,
         humidity: undefined,
         description: undefined,
         error: "Please enter values."
@@ -66,7 +65,7 @@ class App extends React.Component {
       <Weather
        temperature={this.state.temperature}
        city={this.state.city}
-       country={this.state.country}
+	country={this.state.country}
        humidity={this.state.humidity}
        description={this.state.description}
        error={this.state.error}
