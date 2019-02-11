57f2454dd96ab47ac9842868c83665d258f2b258
Romario Williams <romariowilliams@hotmail.com>
2019-02-11 02:10:57 -0500

    Update App.js to use API call from openweathermap.org

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import Weather from "./components/Weather";
 const API_KEY ="354fc5f8ed7e323d84ac3677d76ea348";
 
 class App extends React.Component {
-  getWeather = async () => {
-    const api_call  = await fetch(`api.openweathermap.org/data/2.5/weather?q=&appid=${API_KEY}`);
+  getWeather = async (e) => {
+   e.preventDefault();
+   const city = e.target.elements.city.value;
+   const country = e.target.elements.country.value;
+    const api_call  = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
-    
+    console.log(data);
   }
 
   render(){
     return(
       <div>
       <Titles />
-      <Forms />
+      <Forms getWeather={this.getWeather} />
       <Weather />
       </div>
     );
