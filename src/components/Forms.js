99a647657f93cdb89bb351427857aa29e5151897
Romario Williams <romariowilliams@hotmail.com>
2019-02-11 06:04:08 -0500

    Update Form.js to only require user to enter city name

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 const Forms = props => (
   <form onSubmit={props.getWeather}>
-  <input type="text" name="city" placeholder="City"/>
-  <input type="text" name="country" placeholder="Country"/>
+  <input type="text" name="city" placeholder="City, Country"/>
   <button>Get Weather</button>
   </form>
 );
