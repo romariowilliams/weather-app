2eb90d413226d5f092217b825f1d69a036862f73
Romario Williams <romariowilliams@hotmail.com>
2019-02-11 03:07:03 -0500

    Update Weather component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,6 +4,10 @@ class Weather extends React.Component{
   render(){
     return(
       <div>
+      <p>Location: {this.props.city}, {this.props.country}</p>
+      <p>Temperature: {this.props.temperature}</p>
+      <p>Humidity: {this.props.humidity}</p>
+      <p>Current Conditions: {this.props.description}</p>
       </div>
     );
   }
