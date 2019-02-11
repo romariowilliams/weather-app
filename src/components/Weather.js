c8111ce17633f3b83c9567b2ea0fd59b9472cab4
Romario Williams <romariowilliams@hotmail.com>
2019-02-11 06:04:19 -0500

    Update Weather.js to only require user to enter city name

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Weather = props => (
   <div>
-  { props.city && props.country && <p>Location: {props.city}, {props.country}</p>}
-  { props.temperature && <p>Temperature: {props.temperature}</p>}
-  { props.humidity &&<p>Humidity: {props.humidity}</p>}
+  { props.city&& <p>Location: {props.city}, {props.country}</p>}
+  { props.temperature && <p>Temperature: {props.temperature}&#176;C</p>}
+  { props.humidity &&<p>Humidity: {props.humidity}%</p>}
   { props.description &&<p>Current Conditions: {props.description}</p>}
   { props.error &&<p>{props.error}</p>}
   </div>
