0f17167fb6d17a937664f80f86fd6fce006508ea
Romario Williams <romariowilliams@hotmail.com>
2019-02-11 03:09:49 -0500

    Update Weather component to show results after form is submitted

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,10 +4,10 @@ class Weather extends React.Component{
   render(){
     return(
       <div>
-      <p>Location: {this.props.city}, {this.props.country}</p>
-      <p>Temperature: {this.props.temperature}</p>
-      <p>Humidity: {this.props.humidity}</p>
-      <p>Current Conditions: {this.props.description}</p>
+      { this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>}
+      { this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
+      { this.props.humidity &&<p>Humidity: {this.props.humidity}</p>}
+      { this.props.description &&<p>Current Conditions: {this.props.description}</p>}
       </div>
     );
   }
