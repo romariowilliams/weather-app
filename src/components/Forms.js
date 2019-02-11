9c525f261add0b785fb9194a981ac4fc2e9a54e7
Romario Williams <romariowilliams@hotmail.com>
2019-02-11 02:11:17 -0500

    Update Forms component

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -3,8 +3,11 @@ import React from 'react';
 class Forms extends React.Component{
   render(){
     return(
-      <div>Forms Component
-      </div>
+    <form onSubmit={this.props.getWeather}>
+    <input type="text" name="city" placeholder="City"/>
+    <input type="text" name="country" placeholder="Country"/>
+    <button>Get Weather</button>
+    </form>
     );
   }
 };
