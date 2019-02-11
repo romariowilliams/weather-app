f5e90edee6f748b6718ac471b9683dbe5cc3b0a2
Romario Williams <romariowilliams@hotmail.com>
2019-02-11 05:23:01 -0500

    Update App.js to handle error when city does not exist

--- src/App.js
+++ src/App.js
@@ -21,26 +21,40 @@ class App extends React.Component {
     const api_call  = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
 
-    if (city && country){
-        console.log(data);
-        this.setState({
-        temperature: data.main.temp,
-        city: data.name,
-        country: data.sys.country,
-        humidity: data.main.humidity,
-        description: data.weather[0].description,
-        error: ""
-      });
-    }
-    else{
-      this.setState({
-      temperature: undefined,
-      city: undefined,
-      country: undefined,
-      humidity: undefined,
-      description: undefined,
-      error: "Please enter city and country"
+   if (city && country){
+      	 if(data.name){ // checks if there is data (specifically the city name) fetched from the API
+	
+		this.setState({
+	
+       	 	temperature: data.main.temp,
+       	 	city: data.name,
+       	 	country: data.sys.country,
+        	humidity: data.main.humidity,
+        	description: data.weather[0].description,
+        	error: ""
+	
+      		});
+	}
+	else {
+      		this.setState({
+        	temperature: undefined,
+        	city: undefined,
+        	country: undefined,
+        	humidity: undefined,
+        	description: undefined,
+        	error: "City does not exist."
+     	 });
+     	}
 
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter values."
+      });
     }
   }
 
